39aadab3c1e28d774e2ebbf3f5a55758da0dec07
bernat <andikasukardi1@gmail.com>
2025-09-10 12:27:29 +0900

    ui index.php pembayaran

--- pages/pembayaran/index.php
+++ pages/pembayaran/index.php
@@ -1,3 +1,56 @@
-<?php include_once '../../header.php' ?>
-Pembayaran
-<?php include_once '../../footer.php' ?>
+<?php include_once "../../header.php"; ?>
+
+<div class="flex items-center justify-between mb-6 font-medium">
+    <div class="flex flex-col">
+        <span class="text-2xl">
+            Pembayaran
+        </span>
+        <span class="text-gray-400">
+            Laporan Pembayaran Semester Pendek
+        </span>
+    </div>
+    <div>
+        <button onclick="window.location.href=''" type="button"
+            class="bg-opacity-90 gap-2 px-5 py-2 bg-gray-500 hover:bg-green-800 text-sm text-white rounded-lg">
+            <i class="fas fa-refresh"></i>
+        </button>
+    </div>
+</div>
+<div class="overflow-auto rounded-lg shadow">
+    <table class="w-full">
+        <thead class="bg-gray-50 border-b-2 border-gray-200">
+            <tr>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left w-20">No.</th>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left w-48">NPM Mahasiswa</th>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left">Nama Mahasiswa</th>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left w-24">Total SKS</th>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left w-24">Tagihan</th>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left w-24">Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="odd:bg-white even:bg-gray-50">
+                <td class="p-3 tracking-wider text-sm text-gray-700">1</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">07351811072</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">Andika Sukardi</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">8</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">Rp.1.000.000</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">
+                    <span class="uppercase rounded-lg bg-yellow-300 text-yellow-600 font-medium py-1 px-2 bg-opacity-50">Pending</span>
+                </td>
+            </tr>
+            <tr class="odd:bg-white even:bg-gray-50">
+                <td class="p-3 tracking-wider text-sm text-gray-700">2</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">07351811072</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">Andika Sukardi</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">8</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">Rp.800.000</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">
+                    <span class="uppercase rounded-lg bg-green-300 text-green-600 font-medium py-1 px-2 bg-opacity-50">Selesai</span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<?php include_once "../../footer.php"; ?>
