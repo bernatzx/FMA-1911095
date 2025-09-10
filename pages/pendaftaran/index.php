c281132457bb08c2798a2fd093dba67f2873b0aa
bernat <andikasukardi1@gmail.com>
2025-09-10 12:15:03 +0900

    ui index.php pendaftaran

--- pages/pendaftaran/index.php
+++ pages/pendaftaran/index.php
@@ -1,3 +1,45 @@
-<?php include_once '../../header.php' ?>
-Pendaftaran
-<?php include_once '../../footer.php' ?>
+<?php include_once "../../header.php"; ?>
+
+<div class="flex items-center justify-between mb-6 font-medium">
+    <div class="flex flex-col">
+        <span class="text-2xl">
+            Pendaftaran
+        </span>
+        <span class="text-gray-400">
+            Laporan Pendaftaran Semester Pendek
+        </span>
+    </div>
+    <div>
+        <button onclick="window.location.href=''" type="button"
+            class="bg-opacity-90 gap-2 px-5 py-2 bg-gray-500 hover:bg-green-800 text-sm text-white rounded-lg">
+            <i class="fas fa-refresh"></i>
+        </button>
+    </div>
+</div>
+<div class="overflow-auto rounded-lg shadow">
+    <table class="w-full">
+        <thead class="bg-gray-50 border-b-2 border-gray-200">
+            <tr>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left w-20">No.</th>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left w-48">NPM Mahasiswa</th>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left w-48">Nama Mahasiswa</th>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left">Mata Kuliah</th>
+                <th class="p-3 text-sm font-semibold tracking-wider text-left w-24">Total SKS</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="odd:bg-white even:bg-gray-50">
+                <td class="p-3 tracking-wider text-sm text-gray-700">1</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">07351811072</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">Andika Sukardi</td>
+                <td class="p-3 tracking-wider text-sm text-gray-700">
+                    <div>Linux</div>
+                    <div>Komunikasi Data</div>
+                    <div>Bahasa Inggris</div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<?php include_once "../../footer.php"; ?>
