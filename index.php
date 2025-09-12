efc21ff95eeef28400f7118e1798aa27d7a3ef66
bernat <andikasukardi1@gmail.com>
2025-09-12 21:13:53 +0900

    logika routing index.php

--- index.php
+++ index.php
@@ -3,7 +3,16 @@ require_once "conf.php";
 
 if (!VALID()) {
     header("Location: " . base('auth/'));
-} 
+    exit;
+} else {
+    if (ISADMIN()) {
+        header("Location: " . base('pages/beranda/'));
+        exit;
+    } elseif (ISMHS()) {
+        header("Location: " . base('pages/layoutmhs/'));
+        exit;
+    } else {
+        exit;
+    }
+}
 ?>
-
-Berhasil
