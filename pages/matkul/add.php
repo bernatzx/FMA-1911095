dacb69c9e36dfa811de1d6f60de8a200e7149694
bernat <andikasukardi1@gmail.com>
2025-09-12 23:34:50 +0900

    logika validasi ajax tambah mk

--- pages/matkul/add.php
+++ pages/matkul/add.php
@@ -19,33 +19,90 @@
     </div>
 </div>
 <div class="flex justify-center">
-    <div class="w-[600px] border rounded-lg p-6 shadow">
-        <div class="mb-6">
+    <form class="w-[600px] border rounded-lg p-6 shadow space-y-3" id="addMK">
+        <input type="hidden" name="action" value="addMK">
+        <div class="">
             <div class="font-medium text-sm mb-2">
                 Kode MK :
             </div>
-            <input class="border border-gray-300 bg-gray-200 rounded-sm outline-none p-2 w-full" type="text">
+            <input name="kode" class="border border-gray-300 bg-gray-200 rounded-sm outline-none p-2 w-full" required
+                type="text">
         </div>
-        <div class="mb-6">
+        <div class="">
             <div class="font-medium text-sm mb-2">
                 Mata Kuliah :
             </div>
-            <input class="border border-gray-300 bg-gray-200 rounded-sm outline-none p-2 w-full" type="text">
+            <input name="nama" class="border border-gray-300 bg-gray-200 rounded-sm outline-none p-2 w-full" required
+                type="text">
         </div>
-        <div class="mb-6">
+        <div class="">
             <div class="font-medium text-sm mb-2">
                 SKS :
             </div>
-            <input class="border border-gray-300 bg-gray-200 rounded-sm outline-none p-2 w-full" type="number" min="1" max="4">
+            <input name="sks" class="border border-gray-300 bg-gray-200 rounded-sm outline-none p-2 w-full" required
+                type="number" min="1" max="4">
+        </div>
+        <div id="errorBox"
+            class="hidden font-medium text-sm flex items-center gap-2 p-2 bg-red-400 text-red-800 rounded-sm">
+            <i class="fas fa-circle-info"></i>
+            <span id="errorMsg" class="flex-auto"></span>
+            <div id="closeErrorBoxBtn">
+                <i class="cursor-pointer fas fa-times"></i>
+            </div>
         </div>
         <div class="float-right">
-            <button class="bg-opacity-90 gap-2 px-5 py-2 bg-green-700 hover:bg-green-800 text-sm text-white rounded-lg" type="button">
+            <button class="bg-opacity-90 gap-2 px-5 py-2 bg-green-700 hover:bg-green-800 text-sm text-white rounded-lg"
+                type="submit">
                 <i class="fas fa-add"></i>
                 <span>
                     Tambah
                 </span>
             </button>
         </div>
-    </div>
+    </form>
 </div>
+
+<script>
+    const form = document.getElementById('addMK');
+    const errorBox = document.getElementById('errorBox');
+    const errorMsg = document.getElementById('errorMsg');
+    const closeErrorBoxBtn = document.getElementById('closeErrorBoxBtn');
+
+    if (closeErrorBoxBtn) {
+        closeErrorBoxBtn.addEventListener('click', () => {
+            errorBox.classList.toggle('hidden');
+        })
+    }
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const formData = new FormData(form);
+        const kode = formData.get('kode');
+        const nama = formData.get('nama');
+        const sks = formData.get('sks');
+        if (!kode || !nama || !sks) {
+            errorMsg.textContent = 'Semua field wajib diisi.';
+            errorBox.classList.remove('hidden');
+            return;
+        }
+
+        try {
+            const res = await fetch("pros.php", {
+                method: 'POST',
+                body: formData
+            })
+            const data = await res.json();
+            if (data.success) {
+                window.location = <?= base('pages/matkul/') ?>;
+            } else {
+                errorMsg.textContent = data.msg;
+                errorBox.classList.remove('hidden');
+            }
+        } catch (error) {
+            errorMsg.textContent = 'Terjadi kesalahan koneksi ke server.';
+            errorBox.classList.remove('hidden');
+        }
+    })
+</script>
+
 <?php include_once '../../footer.php'; ?>
