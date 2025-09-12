2102bcb95d317b07e00f4f63915ed5abb46f0698
bernat <andikasukardi1@gmail.com>
2025-09-12 22:50:28 +0900

    respon server untuk daftar pada auth.php

--- auth/auth.php
+++ auth/auth.php
@@ -11,7 +11,7 @@ $action = $_POST['action'] ?? '';
 switch ($action) {
     case 'login':
         $nama = trim(mysqli_real_escape_string($hub, $_POST['nama']));
-        $sandi = trim(mysqli_real_escape_string($hub, $_POST['sandi']));
+        $sandi = mysqli_real_escape_string($hub, $_POST['sandi']);
 
         $sql = mysqli_query($hub, "SELECT * FROM tb_user WHERE nama = '$nama' LIMIT 1") or die(mysqli_error($hub));
         $data = mysqli_fetch_assoc($sql);
@@ -27,6 +27,35 @@ switch ($action) {
                 'success' => false,
                 'msg' => 'Nama Pengguna atau Kata Sandi Salah!'
             ]);
+        }
+        break;
+    case 'reg':
+        $nama = trim(mysqli_real_escape_string($hub, $_POST['nama']));
+        $npm = trim(mysqli_real_escape_string($hub, $_POST['npm']));
+        $sandi = mysqli_real_escape_string($hub, $_POST['sandi']);
+        $konfirmasi = mysqli_real_escape_string($hub, $_POST['konfirmasi']);
+
+        if ($sandi !== $konfirmasi) {
+            echo json_encode(['success' => false, 'msg' => 'Konfirmasi sandi tidak cocok.']);
+            exit;
+        }
+
+        $check = mysqli_query($hub, "SELECT * FROM tb_user WHERE npm = '$npm' LIMIT 1") or die(mysqli_error($hub));
+        if (mysqli_num_rows($check) > 0) {
+            echo json_encode(['success' => false, 'msg' => 'Npm tersebut telah terdaftar.']);
+        } else {
+            $passENC = password_hash($sandi, PASSWORD_DEFAULT);
+            mysqli_query($hub, "INSERT INTO tb_user (npm, nama, kata_sandi, level) VALUES ('$npm', '$nama', '$passENC', 'mhs')") or die(mysqli_error($hub));
+
+            $data = [
+                'npm' => $npm,
+                'nama' => $nama,
+                'level' => 'mhs'
+            ];
+            $_SESSION['valid'] = true;
+            $_SESSION['userData'] = $data;
+
+            echo json_encode(['success' => true]);
         }
         break;
     default:
